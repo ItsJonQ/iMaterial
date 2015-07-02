569017c1c5a3ea3efe5d8828e6999580b7908081
ItsJonQ <itsjonq@gmail.com>
2015-07-01 20:54:58 -0400

    Added styles for segment control button groups

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,14 @@
 console.log('\'Allo \'Allo!');
+
+(function($) { 'use strict';
+
+  var $segmentControl = $('.im-button-group--segment-control');
+
+  $segmentControl.on('click', '.im-button', function() {
+    var $this = $(this);
+    $this.addClass('is-active');
+    $this.siblings().removeClass('is-active');
+  });
+
+
+})(jQuery);
